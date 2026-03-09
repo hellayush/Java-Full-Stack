4aa871d28b2204dbc0d44de15a59c0c688174c84
hellayush <129208635+hellayush@users.noreply.github.com>
2026-03-09 18:44:30 +0530

    Day 1 Basic Java programs

---
+++ Basic-java/Day1/Variables.java
@@ -0,0 +1,46 @@
+package Day1;
+
+
+
+public class Variables {
+
+
+    /**
+     * This is JavaDoc Comment
+     * {@link Stable} to trust
+     */
+
+    public static void main(String[] args) {
+        // Declaration
+        int a;
+        String name;
+
+        // Initialization
+        a = 10;
+        name = "Java";
+
+        int age = 20;
+
+
+        System.out.println("Hello, " + name + ". You are " + age + " years old");
+
+        // Your name, your age, your favourite programming language
+        name = "Faisal";
+        age = 25;
+        String language = "Java";
+
+
+        // CONSTANTS
+        final int CONSTANT_VALUE = 3;
+
+        /*
+        Multi
+        Line
+        Comments
+         */
+
+
+
+
+    }
+}
